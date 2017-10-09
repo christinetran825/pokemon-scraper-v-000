80b996fb825847f2ec78230f93039d0e378d32f0
Christine Tran <christinetran825@gmail.com>
2017-10-09 19:07:17 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,33 +2,33 @@ class Pokemon
 
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id, name, type, db)  #(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
   end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    the_pokemon = @db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    Pokemon.new(id: the_pokemon[0], name: the_pokemon[1], type: the_pokemon[2]
+  end
+
   #
-  # def self.save(name, type, db)
-  #   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  # describe ".find" do
+  #   it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
+  #     # The find method creates a new Pokemon after selecting their row from the database by their id number.
+  #     Pokemon.save("Pikachu", "electric", @db)
+  #
+  #     pikachu_from_db = Pokemon.find(1, @db)
+  #     expect(pikachu_from_db.id).to eq(1)
+  #     expect(pikachu_from_db.name).to eq("Pikachu")
+  #     expect(pikachu_from_db.type).to eq("electric")
+  #   end
   # end
-  #
-  # def self.find(id, db)
-  #   the_pokemon = @db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-  #   Pokemon.new(id: the_pokemon[0], name: the_pokemon[1], type: the_pokemon[2])
-  # end
-  #
-  # #
-  # # describe ".find" do
-  # #   it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
-  # #     # The find method creates a new Pokemon after selecting their row from the database by their id number.
-  # #     Pokemon.save("Pikachu", "electric", @db)
-  # #
-  # #     pikachu_from_db = Pokemon.find(1, @db)
-  # #     expect(pikachu_from_db.id).to eq(1)
-  # #     expect(pikachu_from_db.name).to eq("Pikachu")
-  # #     expect(pikachu_from_db.type).to eq("electric")
-  # #   end
-  # # end
 
 end
