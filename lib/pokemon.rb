e6acf6d148cda1cb619552ac5ed5572f140e1a3d
Christine Tran <christinetran825@gmail.com>
2017-10-09 18:30:27 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,76 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id, name, type, db)
+    binding.pry
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  # def self.save(name, type, db)
+  #   db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+  # end
+  #
+  # def self.find
+  #
+  # end
+
+
 end
+
+#   describe ".save" do
+#     it 'saves an instance of a pokemon with the correct id' do
+#       Pokemon.save("Pikachu", "electric", @db)
+#
+#       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+#       expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+#     end
+#   end
+#
+#   describe ".find" do
+#     it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
+#       # The find method creates a new Pokemon after selecting their row from the database by their id number.
+#       Pokemon.save("Pikachu", "electric", @db)
+#
+#       pikachu_from_db = Pokemon.find(1, @db)
+#       expect(pikachu_from_db.id).to eq(1)
+#       expect(pikachu_from_db.name).to eq("Pikachu")
+#       expect(pikachu_from_db.type).to eq("electric")
+#     end
+#   end
+#
+#   describe "BONUS" do
+#
+#     before do
+#       @sql_runner.execute_create_hp_column
+#       Pokemon.save('Pikachu', 'electric', @db)
+#       Pokemon.save('Magikarp', 'water', @db)
+#     end
+#
+#     let(:pikachu){Pokemon.find(1, @db)}
+#     let(:magikarp){Pokemon.find(2, @db)}
+#
+#     # remove the 'x' before 'it' to run these tests
+#     xit "knows that a pokemon have a default hp of 60" do
+#       # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
+#       # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
+#       expect(@db.execute("SELECT hp FROM pokemon").flatten.first).to eq(60)
+#     end
+#
+#     # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
+#     # He used splash. It wasn't very effective. It did one damage.
+#     xit "alters Pikachu's hp to 59" do
+#       pikachu.alter_hp(59, @db)
+#       expect(Pokemon.find(1, @db).hp).to eq(59)
+#     end
+#
+#     # Now we alter Magikarp's hp
+#     xit "alters Magikarp's hp" do
+#       magikarp.alter_hp(0, @db)
+#       expect(Pokemon.find(2, @db).hp).to eq(0)
+#     end
+#   end
+# end
