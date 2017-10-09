54b1edb10753e773ea89b81cdb902627d4f8dd26
Christine Tran <christinetran825@gmail.com>
2017-10-09 19:16:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,17 +18,4 @@ class Pokemon
     self.new(id: the_pokemon[0], name: the_pokemon[1], type: the_pokemon[2], db: db)
   end
 
-  #
-  # describe ".find" do
-  #   it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
-  #     # The find method creates a new Pokemon after selecting their row from the database by their id number.
-  #     Pokemon.save("Pikachu", "electric", @db)
-  #
-  #     pikachu_from_db = Pokemon.find(1, @db)
-  #     expect(pikachu_from_db.id).to eq(1)
-  #     expect(pikachu_from_db.name).to eq("Pikachu")
-  #     expect(pikachu_from_db.type).to eq("electric")
-  #   end
-  # end
-
 end
